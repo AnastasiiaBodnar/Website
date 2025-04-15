c2e9ef715fab7d8573e6ce11f460741bf0e5f965
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2025-04-15 13:09:32 +0300

    end

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,34 @@ const { src, dest, watch, series } = require('gulp');
 const browserSync = require('browser-sync').create();
 
 function htmlTask() {
-  return src('index.html')
+  return src('src/index.html')
     .pipe(dest('dist'))
     .pipe(browserSync.stream());
 }
 
 function cssTask() {
-  return src('style.css')
+  return src('src/style.css')
     .pipe(dest('dist'))
     .pipe(browserSync.stream());
 }
 
 function imageTask() {
-  return src('background.png', { allowEmpty: true })
-    .pipe(dest('dist'))
+  return src('src/img/**/*', {encoding: false})
+    .pipe(dest('dist/img'))
     .pipe(browserSync.stream());
 }
+
 
 function serve() {
   browserSync.init({
     server: {
-      baseDir: 'dist'
+      baseDir: 'dist',
     }
   });
 
-  watch('index.html', htmlTask);
-  watch('style.css', cssTask);
-  watch('background.png', imageTask); 
+  watch('src/index.html', htmlTask);
+  watch('src/style.css', cssTask);
+  watch('src/img/**/*', imageTask);
 }
 
 exports.default = series(htmlTask, cssTask, imageTask, serve);
